c58a3a9321a369ea1a322c70dbea727d11b38ae9
Buddy Burden <barefootcoder@gmail.com>
2016-12-28 18:49:54 -0800

    update dzil minting profile w/ modern dist changes

--- dzil/profiles/default/Module.pm
+++ dzil/profiles/default/Module.pm
@@ -2,13 +2,27 @@ package {{$name}};
 
 use strict;
 use warnings;
+use autodie ':all';
 
 # VERSION
 
 
+
 1;
+
+
 
 # ABSTRACT:
 # COPYRIGHT
 
-__END__
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 USAGE
+
+
+=head1 BUGS, CAVEATS and NOTES
