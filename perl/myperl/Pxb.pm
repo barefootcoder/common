486aad37c5433114e6ad740d04fad466fd1c7028
Buddy Burden <barefootcoder@gmail.com>
2019-03-27 03:06:15 -0700

    refactor myperl::Pxb's alias pulling functionality
    this way it can be reused for other aliases

--- perl/myperl/Pxb.pm
+++ perl/myperl/Pxb.pm
@@ -3,6 +3,7 @@ package myperl::Pxb;
 use myperl::Script ();
 use List::Util qw< first >;
 
+use CLASS;
 use Path::Class::Tiny;
 use PerlX::bash ':all';
 
@@ -20,8 +21,10 @@ our @EXPORT =
 # (limited unit tests) #
 ########################
 
-# define `ps` however it's defined in my .tcshrc alias
-our @ps = split(' ', first { $_ } map { /alias ps '(.*?)'/ ? $1 : () } path("~/.tcshrc")->slurp);
+# define a command however it's defined in my .tcshrc alias
+sub _get_alias { split(' ', first { $_ } map { /alias $_[1] '(.*?)'/ ? $1 : () } path("~/.tcshrc")->slurp) }
+
+our @ps = CLASS->_get_alias("ps");
 
 # some things will want to know where our timerfile is
 our $timerfile = path('~/timer/timer-new');
